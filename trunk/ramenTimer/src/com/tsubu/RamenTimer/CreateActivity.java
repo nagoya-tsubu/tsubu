2910fd237a10630b6d29e1a03ec74c2fc8e97845
miguse@gmail.com <miguse@gmail.com@dc9ad784-bbf2-a1e6-7a43-420780131b40>
2010-10-30 08:10:05 +0000

    各アクティビディの呼出のボタンを作りましたので使ってあげてください。

---
+++ trunk/ramenTimer/src/com/tsubu/RamenTimer/CreateActivity.java
@@ -0,0 +1,13 @@
+package com.tsubu.RamenTimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateActivity extends Activity  {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_activity);
+	}
+}
