54807d660ec1ce629f78c33b20125f9aba65e7bc
leibun@gmail.com <leibun@gmail.com@dc9ad784-bbf2-a1e6-7a43-420780131b40>
2010-10-31 01:57:20 +0000

    ラーメンタイマープロジェクトの登録画面を作成 インテントを使ってバーコードリーダーからJANコードを読み取って、EditTextに反映させるようにしました。 参考サイト：http://d.hatena.ne.jp/mowamowa/20100208/1265600982

--- ramenTimer/src/com/tsubu/ramentimer/CreateActivity.java
+++ ramenTimer/src/com/tsubu/ramentimer/CreateActivity.java
@@ -1,15 +1,57 @@
 package com.tsubu.ramentimer;
 
-import com.tsubu.ramentimer.R;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-public class CreateActivity extends Activity  {
+public class CreateActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_activity);
+		setContentView(R.layout.create_activity);
+
+		ImageButton captureButton = (ImageButton) findViewById(R.id.CaptureButton);
+		captureButton.setOnClickListener(captureClick);
+	}
+
+	/**
+	 * キャプチャーボタンが押された時の動作 インテントでバーコードリーダーを呼び出しに行く
+	 */
+	final OnClickListener captureClick = new OnClickListener() {
+		public void onClick(View v) {
+			Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+			// intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+			intent.putExtra("SCAN_MODE", "ONE_D_MODE");
+			try {
+				startActivityForResult(intent, 0);
+			} catch (ActivityNotFoundException e) {
+				new AlertDialog.Builder(CreateActivity.this).setTitle(
+						"QR Scaner not found.").setMessage(
+						"Please install QR code scanner").setPositiveButton(
+						"OK", null).show();
+			}
+		}
+	};
+
+	/**
+	 * インテントがもどってきた時の動作
+	 */
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0) {
+			//EditTextにバーコードリーダーで読み込んだJANコードの値を設定
+			EditText jancode = (EditText) findViewById(R.id.JanEdit);
+			if (resultCode == RESULT_OK) {
+				final String barcode = intent.getStringExtra("SCAN_RESULT");
+				jancode.setText(barcode);
+			}
+		}
 	}
 }
