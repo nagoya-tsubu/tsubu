c7d03cadc160ae2a4b625bf0c8f997d74c858670
leibun@gmail.com <leibun@gmail.com@dc9ad784-bbf2-a1e6-7a43-420780131b40>
2010-11-01 06:44:41 +0000

    @leibun ・麺の種類の選択をRadioButtonで実装（in CreateActivity.java, create_activity.xml） 文字列がNoodleType.javaの中にあったので、RadioGroupだけxmlに作っておいて、 RadioButtonはCreateActivity.onCreate()内で実装
    ・jarCodeの型をintからStringに変更（in NoodleHistory.java, NoodleMaster.java）
    
    ・UI周りを色々試行錯誤中（in create_activity.xml）
    アクションバーにボタンを集めてみたけどイマイチ、あとで考え直す
    	・アクションバーをクリック時の画像変化をSelectorで実装
    	追加ファイル camera_button.xml,play_button.xml,image_button.xml 他png
    	・アクションバーのクリックイベントを追加
    	登録ボタン（再生マーク）：onCreateClick 登録動作（まだNoodleMasterをインスタンス化するところまで）
    	画像マークのボタン：onLoadImageClick ギャラリーが起動（ギャラリーとカメラを選択できるようにダイアログか何かを出す予定）
    	カメラマークのボタン：onCaptureClick バーコードリーダーが起動
    
    ・ImageButtonからImageViewにボタンを変更したら @tan1234jp さんがやってくれた、最初のフォーカスが効かなくなってしまったｗｗｗ

--- ramenTimer/src/com/tsubu/ramentimer/CreateActivity.java
+++ ramenTimer/src/com/tsubu/ramentimer/CreateActivity.java
@@ -1,65 +1,87 @@
 package com.tsubu.ramentimer;
 
+import java.io.InputStream;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-//import android.view.View.OnClickListener;
 import android.widget.EditText;
-//import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class CreateActivity extends Activity {
 
 	//QRコードスキャナーのパッケージ名
 	private static final String	QRCODE_PKG_NAME = "com.google.zxing.client.android";
+	private static final int REQUEST_BARCODE = 0;
+	private static final int REQUEST_GALLERY = 1;
+	
+	//JANコード
+	private EditText janEdit = null;
+	//商品名
+	private EditText nameEdit = null;
+	//商品説明
+	private EditText descriptionEdit = null;
+	//ゆで時間
+	private EditText boilTimeEdit = null;
+	//麺の種類
+	private RadioGroup noodleTypeRadioGroup = null;
+	//商品の画像
+	private ImageView noodleImageView = null;
+	private Bitmap noodleImage = null;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_activity);
-/*
-		ImageButton captureButton = (ImageButton) findViewById(R.id.CaptureButton);
-		captureButton.setOnClickListener(captureClick);
-*/
+		//ボタンとかエディットボックスとかのUIを取ってくる
+		janEdit = (EditText) findViewById(R.id.JanEdit);
+		nameEdit= (EditText) findViewById(R.id.NameEdit);
+		descriptionEdit=(EditText) findViewById(R.id.DescriptionEdit);
+		boilTimeEdit = (EditText) findViewById(R.id.BoilingTimeEdit);
+		noodleTypeRadioGroup	= (RadioGroup) findViewById(R.id.NoodleTypeRadioGroup);
+		noodleImageView = (ImageView) findViewById(R.id.NoodleImageView);
+		
+		//麺の種類をラジオボタンで作成
+		final NoodleType NoodleTypeValues[] = NoodleType.values();
+		for(int i=0;i<NoodleTypeValues.length;i++){
+			RadioButton radioButton=new RadioButton(this);
+			radioButton.setText(NoodleTypeValues[i].getName());
+			radioButton.setId(i);
+			noodleTypeRadioGroup.addView(radioButton);
+		}
 	}
 
-	/**
-	 * キャプチャーボタンが押された時の動作 インテントでバーコードリーダーを呼び出しに行く
-	 */
-/*
-	final OnClickListener captureClick = new OnClickListener() {
-		public void onClick(View v) {
-			Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-			// intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-			intent.putExtra("SCAN_MODE", "ONE_D_MODE");
-			try {
-				startActivityForResult(intent, 0);
-			} catch (ActivityNotFoundException e) {
-				new AlertDialog.Builder(CreateActivity.this).setTitle(
-						"QR Scaner not found.").setMessage(
-						"Please install QR code scanner").setPositiveButton(
-						"OK", null).show();
-			}
-		}
-	};
-*/
 
 	/**
 	 * インテントがもどってきた時の動作
 	 */
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		if (requestCode == 0) {
-			//EditTextにバーコードリーダーで読み込んだJANコードの値を設定
-			EditText jancode = (EditText) findViewById(R.id.JanEdit);
-			if (resultCode == RESULT_OK) {
-				final String barcode = intent.getStringExtra("SCAN_RESULT");
-				jancode.setText(barcode);
-			}
+		if (resultCode == RESULT_OK) {
+			if (requestCode == REQUEST_BARCODE) {
+				//EditTextにバーコードリーダーで読み込んだJANコードの値を設定
+					final String barcode = intent.getStringExtra("SCAN_RESULT");
+					janEdit.setText(barcode);
+			}else if(requestCode == REQUEST_GALLERY){
+				try{
+					InputStream is = getContentResolver().openInputStream(intent.getData());
+					noodleImage = BitmapFactory.decodeStream(is);
+					is.close();
+					noodleImageView.setImageBitmap(noodleImage);
+				}catch(Exception e){
+					Log.e("CreateActivity", "Intent return error by REQUEST_GALLARY"+e.toString());
+				}    
+	        }
 		}
 	}
 	
@@ -76,7 +98,7 @@ public class CreateActivity extends Activity {
 
 		//QRコードスキャナーを呼び出す
 		try {
-			startActivityForResult(intent, 0);
+			startActivityForResult(intent, REQUEST_BARCODE);
 		} catch(ActivityNotFoundException e) {
 			//アクティビティが存在しない(=インテントの開始に失敗した)場合は、
 			//Android Marketからダウンロードするか問い合わせる
@@ -101,4 +123,58 @@ public class CreateActivity extends Activity {
 					.create().show();
 		}
 	}
+	
+	/**
+	 * 画像読み込みボタンが押された時の動作 インテントでギャラリーを呼び出す
+	 * @param v
+	 */
+	public void onLoadImageClick(View v) {
+		Intent intent = new Intent();
+		intent.setType("image/*");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		// 第二引数は戻ってきたときの判別用の適当なint
+		try {
+			startActivityForResult(intent, REQUEST_GALLERY);
+		} catch (ActivityNotFoundException e) {
+			new AlertDialog.Builder(CreateActivity.this).setTitle(
+					"Gallery not found.").setMessage(
+					"Please install Gallery").setPositiveButton(
+					"OK", null).show();
+		}
+	}
+	
+	/**
+	 * 登録ボタンが押された時の動作
+	 */
+	public void onCreateClick(View v) {
+//		try{
+			NoodleMaster noodleMaster = getNoodleMaster();
+//		}catch(Exception e){
+			
+//		}
+	}
+	
+	/**
+	 * logoボタンが押された時の動作
+	 */
+	public void onLogoClick(View v) {
+		finish();
+	}
+
+	/**
+	 * UIから登録情報を集めて返す
+	 * @return
+	 */
+	NoodleMaster getNoodleMaster(){
+		//EditTextやRadioGroupから状態を取得
+		String jancode = janEdit.getText().toString();
+		String name = nameEdit.getText().toString();
+		String description = descriptionEdit.getText().toString();
+		int boilTime = new Integer(boilTimeEdit.getText().toString()).intValue();
+		Bitmap image = noodleImage;
+		NoodleType noodleType = NoodleType.values()[noodleTypeRadioGroup.getCheckedRadioButtonId()];
+		NoodleMaster noodle = new NoodleMaster(jancode,name,image,boilTime,noodleType);
+		return noodle;
+	}
+	
 }
