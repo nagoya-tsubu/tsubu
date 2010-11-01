ca78ce801a4860990a5099a71856e18e5c00c0a2
Ikuo.Tansho@gmail.com <Ikuo.Tansho@gmail.com@dc9ad784-bbf2-a1e6-7a43-420780131b40>
2010-11-01 02:55:13 +0000

    @tan1234jp 【修正】 ・AndroidManifest.xml 　(1)SDKの使用バージョンを明記した ・CreateActivity.java 　(1)QRコードスキャナーがインストールされていない場合、Android Marketから 　   ダウンロードするようにした 　(2)QRコードスキャナーボタンのクリックイベントを関数化した ・create_activity.xml 　(1)バーコードのEditTextの文字列上限を13文字とした。 　(2)QRコードスキャナーボタンのクリックイベントを追加した。 　(3)最初のフォーカスをQRコードスキャナーボタンとした。 ・ramentimer.asta(UML) 　(1)QRコード読み取り部分のシーケンス図を追加

--- ramenTimer/src/com/tsubu/ramentimer/CreateActivity.java
+++ ramenTimer/src/com/tsubu/ramentimer/CreateActivity.java
@@ -3,27 +3,34 @@ package com.tsubu.ramentimer;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
+//import android.view.View.OnClickListener;
 import android.widget.EditText;
-import android.widget.ImageButton;
+//import android.widget.ImageButton;
 
 public class CreateActivity extends Activity {
 
+	//QRコードスキャナーのパッケージ名
+	private static final String	QRCODE_PKG_NAME = "com.google.zxing.client.android";
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_activity);
-
+/*
 		ImageButton captureButton = (ImageButton) findViewById(R.id.CaptureButton);
 		captureButton.setOnClickListener(captureClick);
+*/
 	}
 
 	/**
 	 * キャプチャーボタンが押された時の動作 インテントでバーコードリーダーを呼び出しに行く
 	 */
+/*
 	final OnClickListener captureClick = new OnClickListener() {
 		public void onClick(View v) {
 			Intent intent = new Intent("com.google.zxing.client.android.SCAN");
@@ -39,6 +46,7 @@ public class CreateActivity extends Activity {
 			}
 		}
 	};
+*/
 
 	/**
 	 * インテントがもどってきた時の動作
@@ -52,6 +60,45 @@ public class CreateActivity extends Activity {
 				final String barcode = intent.getStringExtra("SCAN_RESULT");
 				jancode.setText(barcode);
 			}
+		}
+	}
+	
+	/**
+	 * バーコードをキャプチャーする時に呼び出される
+	 * @param view
+	 */
+	public void onCaptureClick(View view) {
+
+		//QRコードスキャナーのインテントを設定する
+		final Intent intent = new Intent(QRCODE_PKG_NAME + ".SCAN");
+		//JANコードを読み取る
+		intent.putExtra("SCAN_MODE", "ONE_D_MODE");
+
+		//QRコードスキャナーを呼び出す
+		try {
+			startActivityForResult(intent, 0);
+		} catch(ActivityNotFoundException e) {
+			//アクティビティが存在しない(=インテントの開始に失敗した)場合は、
+			//Android Marketからダウンロードするか問い合わせる
+			
+			//TODO: メッセージ文字列のリソース化
+			
+			new AlertDialog.Builder(this)
+					.setTitle("QR Code Scanner not found.")
+					.setMessage("QRコードスキャナーをAndroid Marketからインストールしますか？")
+					.setPositiveButton("はい", new DialogInterface.OnClickListener() {
+						//「はい」押下時は、Android Marketへ飛び、QRコードスキャナーの
+						//ダウンロードページを表示する
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							final Intent intent = new Intent(
+									Intent.ACTION_VIEW,
+									Uri.parse("market://search?q=pname:" + QRCODE_PKG_NAME));
+							startActivity(intent);
+						}
+					})
+					.setNegativeButton("いいえ", null)
+					.create().show();
 		}
 	}
 }
