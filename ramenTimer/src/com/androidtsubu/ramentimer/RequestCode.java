98f3bd754ef80d45d14537da014b60439a076c4a
leibun@gmail.com <leibun@gmail.com@dc9ad784-bbf2-a1e6-7a43-420780131b40>
2010-11-03 08:26:18 +0000

    @leibun ・画面遷移をリクエストコードで管理するように変更（追加 RequestCode.java） RequestCode.javaの中身はEnumになってます。 詳しくは共有ドキュメントの「ソーシャルラーメンタイマー画面遷移時の呼び出し元の判別方法」を見てください。 もっとスマートな方法があったら誰か教えて〜
    ・呼び出し元がReaderActivityとTimerActivityで登録画面の登録ボタンを押した時の動作が変わるように変更（in CreateActivity.java）
    Timerからの場合はCreateActivityは終了
    Readerからの場合は「タイマーを起動」か「ダッシュボードに戻る」をダイアログで選択できるようにした。

---
+++ ramenTimer/src/com/androidtsubu/ramentimer/RequestCode.java
@@ -0,0 +1,23 @@
+package com.androidtsubu.ramentimer;
+
+/**
+ * リクエストコードの種類
+ * 呼び出し元の分岐を管理
+ * @author leibun
+ *
+ */
+public enum RequestCode {
+	DASHBORAD2READER,		//ダッシュボードからリーダー
+	DASHBORAD2HISTORY,	//ダッシュボードから履歴
+	DASHBORAD2TIMER,		//ダッシュボードからタイマー
+	READER2TIMER,			//リーダーからタイマー
+	READER2CREATE,		//リーダーから登録
+	CREATE2TIMER,			//登録からタイマー
+	TIMER2CREATE,			//タイマーから登録
+	HISTORY2TIMER;		//履歴からタイマー
+	
+	//Intent.putExtra()の第１引数に利用
+	public static final String RESUEST_CODE= "REQUEST_CODE";
+	
+};
+
