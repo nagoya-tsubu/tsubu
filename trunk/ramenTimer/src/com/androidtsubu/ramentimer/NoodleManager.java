b56b400eef28b1e73628b28021d85a7a5fa06f91
hidepon.mory@gmail.com <hidepon.mory@gmail.com@dc9ad784-bbf2-a1e6-7a43-420780131b40>
2010-11-18 16:47:03 +0000

    image画像保存先をSDに変更しました 原因不明のエラーあり＞＜

--- trunk/ramenTimer/src/com/androidtsubu/ramentimer/NoodleManager.java
+++ trunk/ramenTimer/src/com/androidtsubu/ramentimer/NoodleManager.java
@@ -1,9 +1,12 @@
 package com.androidtsubu.ramentimer;
 
+import java.io.File;
 import java.sql.SQLException;
 import java.util.Date;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.os.Environment;
 
 /**
  * データの読み書きをするマネージャーです
@@ -16,6 +19,9 @@ public class NoodleManager {
 	private NoodleSqlController noodleSqlController;
 	/** GAE読み書きクラス */
 	private NoodleGaeController noodleGaeController;
+	private Context context;
+	/** image画像保存ディレクトリ */
+	private File directory;
 
 	/**
 	 * コンストラクタ
@@ -23,8 +29,11 @@ public class NoodleManager {
 	 * @param context
 	 */
 	public NoodleManager(Context context) {
-		noodleGaeController = new NoodleGaeController(context);
-		noodleSqlController = new NoodleSqlController(context);
+		this.context = context;
+		checkExternalStorage();
+		noodleGaeController = new NoodleGaeController(context,directory);
+		noodleSqlController = new NoodleSqlController(context,directory);
+		
 	}
 
 	/**
@@ -33,20 +42,21 @@ public class NoodleManager {
 	 * @param janCode
 	 * @return
 	 */
-	public NoodleMaster getNoodleMaster(String janCode) throws SQLException, GaeException {
-		//SQliteに商品マスタがあるか問い合わせる
+	public NoodleMaster getNoodleMaster(String janCode) throws SQLException,
+			GaeException {
+		// SQliteに商品マスタがあるか問い合わせる
 		NoodleMaster master = noodleSqlController.getNoodleMaster(janCode);
-		if(master != null){
-			//SQliteの商品マスタを返す
+		if (master != null) {
+			// SQliteの商品マスタを返す
 			return master;
 		}
-		//GAEに問い合わせる
+		// GAEに問い合わせる
 		master = noodleGaeController.getNoodleMaster(janCode);
-		if(master != null){
-			//SQliteになくてGAEにある場合はSQliteに登録してあげる
+		if (master != null) {
+			// SQliteになくてGAEにある場合はSQliteに登録してあげる
 			noodleSqlController.createNoodleMater(master);
 		}
-		return master; 
+		return master;
 	}
 
 	/**
@@ -57,17 +67,18 @@ public class NoodleManager {
 	 * @throws DuplexNoodleMasterException
 	 * @throws GaeException
 	 */
-	public void createNoodleMaster(NoodleMaster noodleMaster) 
+	public void createNoodleMaster(NoodleMaster noodleMaster)
 			throws SQLException, DuplexNoodleMasterException, GaeException {
-		//SQliteにすでに登録されていないか調べる
-		NoodleMaster master = noodleSqlController.getNoodleMaster(noodleMaster.getJanCode());
-		if(master != null){
-			//重複エラーを返す
+		// SQliteにすでに登録されていないか調べる
+		NoodleMaster master = noodleSqlController.getNoodleMaster(noodleMaster
+				.getJanCode());
+		if (master != null) {
+			// 重複エラーを返す
 			throw new DuplexNoodleMasterException();
 		}
-		//SQliteに登録
+		// SQliteに登録
 		noodleSqlController.createNoodleMater(noodleMaster);
-		//GAEに登録
+		// GAEに登録
 		noodleGaeController.create(noodleMaster);
 	}
 
@@ -77,9 +88,26 @@ public class NoodleManager {
 	 * @param noodleMaster
 	 * @throws SQLException
 	 */
-	public void createNoodleHistory(NoodleMaster noodleMaster, Date measureTime){
-		//商品マスタは何かしら登録されてから使われているはずなのでGAEにマスタを登録することはしない
+	public void createNoodleHistory(NoodleMaster noodleMaster, Date measureTime) {
+		// 商品マスタは何かしら登録されてから使われているはずなのでGAEにマスタを登録することはしない
 		noodleSqlController.createNoodleHistory(noodleMaster, measureTime);
+	}
+
+	/**
+	 * 外部ストレージチェック
+	 */
+	private void checkExternalStorage() {
+		String status = Environment.getExternalStorageState();
+		if (status.equals(Environment.MEDIA_MOUNTED)) {
+			directory = new File(Environment.getExternalStorageDirectory(),
+					context.getPackageName());
+			if (!directory.exists()) {
+				directory.mkdirs();
+			}
+		} else {
+			new AlertDialog.Builder(context).setMessage("SDカードを挿入してください")
+					.setPositiveButton("OK", null).show();
+		}
 	}
 
 }
