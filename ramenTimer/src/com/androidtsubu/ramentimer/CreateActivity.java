9d3b308df4f256e0931a19109312eb0f0bb28c98
leibun@gmail.com <leibun@gmail.com@dc9ad784-bbf2-a1e6-7a43-420780131b40>
2010-12-07 09:58:04 +0000

    @leibun ・登録ボタンを押されたときにソフトウェアキーボードを非表示にするように変更（in CreateActivity.java）

--- ramenTimer/src/com/androidtsubu/ramentimer/CreateActivity.java
+++ ramenTimer/src/com/androidtsubu/ramentimer/CreateActivity.java
@@ -6,10 +6,8 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import android.app.Activity;
-import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
@@ -24,13 +22,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup.LayoutParams;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -529,6 +526,11 @@ public class CreateActivity extends Activity {
 	 * @param v
 	 */
 	public void onCreateClick(View v) {
+		// ソフトウェアキーボードを非表示にする
+		InputMethodManager inputMethodManager =   
+             (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);  
+		inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);  
+       
 		try {
 			noodleMaster = getNoodleMaster();
 		} catch (Exception e) {
